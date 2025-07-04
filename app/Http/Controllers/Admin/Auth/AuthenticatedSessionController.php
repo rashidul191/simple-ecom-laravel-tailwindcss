c69605ce5faec0610e033c67e2746af5950b4e83
rashidul191 <rashidul.191cse.gub@gmail.com>
2025-07-04 22:49:08 +0600

    admin login all options done

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\AdminLoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -23,13 +23,13 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(AdminLoginRequest $request): RedirectResponse
     {
-
         $request->authenticate();
 
         $request->session()->regenerate();
 
+        // return redirect()->route('admin.dashboard');
         return redirect()->intended(route('admin.dashboard', absolute: false));
     }
 
@@ -38,12 +38,12 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
-        Auth::guard('web')->logout();
+        Auth::guard('admin')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/admin/login');
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,6 +11,17 @@
                 <div class="p-6 text-gray-900">
                     {{ __("Admin are logged in!") }}
                     <h3 class="text-2xl font-bold">Admin Dashboard</h3>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('admin.logout') }}">
+                        @csrf
+
+                        <x-dropdown-link :href="route('admin.logout')"
+                            onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
                 </div>
             </div>
         </div>
